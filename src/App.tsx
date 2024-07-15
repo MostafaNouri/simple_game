6870429ce9aa0db69f06ae7b0c61f96ba8ce661e
Mostafa <mostafana1375@gmail.com>
2024-07-16 01:53:41 +0330

    User information

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,36 @@ import './App.css'
 import WebApp from "@twa-dev/sdk";
 
 function App() {
-    const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
+    const [userData, setUserData] = useState({
+        id: null,
+        is_bot: false,
+        first_name: '',
+        last_name: '',
+        username: '',
+        language_code: '',
+        is_premium: false,
+        added_to_attachment_menu: false,
+        allows_write_to_pm: false,
+        photo_url: ''
+    });
+
+    const handleClick = () => {
+        WebApp.showAlert(
+            <div>
+                <span>id: {WebApp.WebAppUser.id}</span>
+                <span>is_bot: {WebApp.WebAppUser.is_bot}</span>
+                <span>first_name: {WebApp.WebAppUser.first_name}</span>
+                <span>last_name: {WebApp.WebAppUser.last_name}</span>
+                <span>username: {WebApp.WebAppUser.username}</span>
+                <span>language_code: {WebApp.WebAppUser.language_code}</span>
+                <span>is_premium: {WebApp.WebAppUser.is_premium}</span>
+                <span>added_to_attachment_menu: {WebApp.WebAppUser.added_to_attachment_menu}</span>
+                <span>allows_write_to_pm: {WebApp.WebAppUser.allows_write_to_pm}</span>
+                <span>photo_url: {WebApp.WebAppUser.photo_url}</span>
+            </div>
+        )
+    }
 
     return (
         <>
@@ -23,8 +52,8 @@ function App() {
                     count is {count}
                 </button>
                 <div className="card">
-                    <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-                        Show Alert
+                    <button onClick={handleClick}>
+                        Show User Information
                     </button>
                 </div>
                 <p>
