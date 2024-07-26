74fe3713782c33e99caf3c0f7311fe0d46ef748b
Mostafa <mostafana1375@gmail.com>
2024-07-27 02:11:34 +0330

    Remove unused code

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,7 @@
 import {useState} from 'react'
 import './App.css'
-import WebApp from "@twa-dev/sdk";
 
 function App() {
-    const [count, setCount] = useState(0);
-    // const [userData, setUserData] = useState({
-    //     id: null,
-    //     is_bot: false,
-    //     first_name: '',
-    //     last_name: '',
-    //     username: '',
-    //     language_code: '',
-    //     is_premium: false,
-    //     added_to_attachment_menu: false,
-    //     allows_write_to_pm: false,
-    //     photo_url: ''
-    // });
-
-    const handleClick = () => {
-        // let webAppData = WebApp.initDataUnsafe;
-        // const data = JSON.parse('{"' + decodeURI(webAppData.replace(/&/g, "\",\"").replace(/=/g,"\":\"")) + '"}');
-        console.log(WebApp.initDataUnsafe);
-        // WebApp.showAlert(
-        //     <div>
-        //         <span>id: {WebApp.WebAppUser.id}</span>
-        //         <span>is_bot: {WebApp.WebAppUser.is_bot}</span>
-        //         <span>first_name: {WebApp.WebAppUser.first_name}</span>
-        //         <span>last_name: {WebApp.WebAppUser.last_name}</span>
-        //         <span>username: {WebApp.WebAppUser.username}</span>
-        //         <span>language_code: {WebApp.WebAppUser.language_code}</span>
-        //         <span>is_premium: {WebApp.WebAppUser.is_premium}</span>
-        //         <span>added_to_attachment_menu: {WebApp.WebAppUser.added_to_attachment_menu}</span>
-        //         <span>allows_write_to_pm: {WebApp.WebAppUser.allows_write_to_pm}</span>
-        //         <span>photo_url: {WebApp.WebAppUser.photo_url}</span>
-        //     </div>
-        // )
-    }
-
     const [width, setWidth] = useState(1000);
 
     // Function to increase the width by 10% on button click
